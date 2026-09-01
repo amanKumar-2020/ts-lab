2328027a4d3df53ba88219ffd7b6b643d41697d0
Aman kumar <jnvaman4@gmail.com>
2026-09-01 23:00:35 +0530

    Refactor customMap function and implement customFilter for array filtering

--- sections/07_Generics/customMap.ts
+++ sections/07_Generics/customMap.ts
@@ -1,9 +1,23 @@
 // create a generic TypeScript function that takes an array and a callback, applies the callback to every element, and returns a new array.
 
-function customMap<T, U>(arr: T[], cb: (item: T) => U): U[] {
-  return arr.map(cb);
+// function customMap<T, U>(arr: T[], cb: (item: T) => U): U[] {
+//   return arr.map(cb);
+// }
+
+// const result = customMap([1, 2], (item) => item * 2);
+
+// console.log(result);
+
+function customFilter<T>(
+  arr: T[],
+  // cb: (el: T, index: number, arr: T[]) => boolean,
+) {
+  const result = [];
+  for (let i= 0; i< arr.length; i++){
+    if(i<3) result.push(i);
+  }
+  return result
 }
 
-const result = customMap([1, 2], (item) => item * 2);
-
-console.log(result);
+const filtered = customFilter([1, 2, 3, 4, 5, 6]);
+console.log(filtered);
