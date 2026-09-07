8f1155291b3e5ed452bc57c925ba2e2ac482c171
Aman kumar <jnvaman4@gmail.com>
2026-09-07 23:39:44 +0530

    Add Store interface and implementation for string transformation

---
+++ sections/07_Generics/methods.ts
@@ -0,0 +1,15 @@
+interface Store<T>{
+    list:T[];
+    transformString<U>(index:number,cb:(item:T)=>U):U;
+}
+
+const store:Store<string> ={
+    list:["Aman" , "Ram" , "Rohan"],
+    transformString(index:number,cb){
+        return cb(this.list[index])
+    }
+}
+
+const result = store.transformString(2,(item)=>item.toLocaleUpperCase)
+
+console.log(result);
